822cf8fcf467c7a8089668338d57eb44bc3017ec
Kasikov <qsefthuken@gmail.com>
2019-08-02 14:52:14 +0700

    get array points of centres

--- src/services/decode.js
+++ src/services/decode.js
@@ -1,11 +1,77 @@
 import vsvgPaths from 'vsvg-paths';
 import earcut from 'earcut';
 
+const TARGET_PERCENT = 9;
+
+const getMedium = (x1, x2, x3) => {
+  return Number((x1 + x2 + x3) / 3);
+}
+
+const getSquare = (x1, y1, x2, y2, x3, y3) => {
+  var points = [(x1 - x3), (x2 - x3), (y1 - y3), (y2 - y3)];
+  return Number(((points[0] * points[3]) - (points[1] * points[2])) / 2);
+}
+
+const sortFormula = (a, b) => {
+  return b.square - a.square;
+}
+
+const sortBySquare = (arPointsCenters) => {
+  arPointsCenters.sort((a, b) => {
+    if (a && a[2] && a[2].square && b && b[2] && b[2].square) {
+      return sortFormula(a[2], b[2]);
+    }
+  })
+
+  return arPointsCenters;
+}
+
+//ФОРМИРУЕМ МАССИВ ИЗ КООРДИНАТ ЦЕНТРОВ КАЖДОГО ТРЕУГОЛЬНИКА
+const getPointCenters = (triangles) => {
+  let arPointsCenters = [];
+
+  let a = 0, b = 1, c = 2;
+  if (triangles[a] && triangles[b] && triangles[c]) {
+    arPointsCenters.push({
+      x: getMedium(
+        triangles[a][0],
+        triangles[b][0],
+        triangles[c][0]
+      )
+    });
+
+    arPointsCenters.push({
+      y: getMedium(
+        triangles[a][1],
+        triangles[b][1],
+        triangles[c][1]
+      )
+    });
+
+    arPointsCenters.push({
+      square: getSquare(
+        triangles[a][0], triangles[a][1],
+        triangles[b][0], triangles[b][1],
+        triangles[c][0], triangles[c][1]
+      )
+    });
+  }
+
+  return arPointsCenters;
+}
+
 // ПОЛУЧАЕМ КООРДИАНТЫ ТОЧЕК ТРЕУГОЛЬНИКОВ
 const trianglePolygons = (arCoords) => {
   // ВЫЗЫВАЕМ ФУНКЦИЮ ТРИАНГУЛЯЦИИ
   const trianglesData = earcut.flatten([arCoords]);
-  return earcut(trianglesData.vertices, trianglesData.holes, trianglesData.dimensions);
+  const trianglesResult = earcut(trianglesData.vertices, trianglesData.holes, trianglesData.dimensions);
+
+  const triangles = trianglesResult.map((trian, trianIndex) => {
+    const index = trianglesResult[trianIndex];
+    return [trianglesData.vertices[index * trianglesData.dimensions], trianglesData.vertices[index * trianglesData.dimensions + 1]];
+  });
+
+  return triangles;
 }
 
 // ФОРМИРУЕМ МАССИВ ИЗ ОБЪЕКТОВ В МАССИВ ИЗ МАССИВОВ (для earcut)
@@ -32,8 +98,17 @@ const decodePaths = (arData, isAbsoluteCoord) => {
     return trianglePolygons(coords);
   });
 
-  console.log(arAbsoluteData);
-  console.log(trian);
+  const arPointsCenters = trian.map(items => {
+    return getPointCenters(items);
+  })
+
+  // закончил тут
+  // теперь нужно получить 9% количества точек от каждого полигона
+  // и затем нарисовать точки
+  const arSortPointCenters = sortBySquare(arPointsCenters);
+
+  // console.log(arAbsoluteData);
+  // console.log(arPointsCenters);
 }
 
 // ПРИВОДИМ КООРДИНАТЫ К АБСОЛЮТНЫМ ЗНАЧЕНИЯМ
